5ad91475c2a0d15f501e2e9fe4ed33a392c30137
Do Park <do.park@dronedeploy.com>
2023-06-02 08:25:17 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import Box from './components/Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ width: '100%', height: '100vh', backgroundColor: '#000' }}>
+    <Canvas>
+      <directionalLight intensity={0.5} />
+      <ambientLight intensity={0.5} />
+       <Box/>
+    </Canvas>
+  </div>
   );
 }
 
